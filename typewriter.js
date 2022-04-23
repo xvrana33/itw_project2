ae9af823d443ca6734e11911c06d456c1937b744
xvrana33 <100077574+xvrana33@users.noreply.github.com>
2022-04-23 20:19:51 +0200

    Add files via upload

---
+++ typewriter.js
@@ -0,0 +1,15 @@
+var messageArray = ["I'm Bilbo Baggins"];
+var textPosition = 0;
+var speed = 100;
+
+
+typewriter = () => {
+document.querySelector("#hello").
+innerHTML = messageArray[0].substring(0,
+textPosition) + "<hellospan>\u25ae</hellospan>";
+
+if(textPosition++ != messageArray[0].length)
+setTimeout (typewriter, speed);
+}
+
+window.addEventListener("load", typewriter);
